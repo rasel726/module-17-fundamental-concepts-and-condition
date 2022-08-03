5a76dd893d4bc18d031d6ab18304be65754de610
rasel726 <md.rasel2726@gmail.com>
2022-08-04 01:39:48 +0600

    push pop file added

---
+++ push-pop.js
@@ -0,0 +1,15 @@
+var numbers = [78, 45, 98, 45];
+// use push to add element to an array as the last element array
+// numbers.push(61);
+// console.log(numbers);
+var friends = ['balam', 'kalam', 'rubel'];
+friends.push('rasel');
+friends.push('tamal');
+// console.log(friends);
+
+// use pop to get last element
+numbers.pop();
+var element = numbers.pop();
+console.log(numbers);
+console.log(element);
+
