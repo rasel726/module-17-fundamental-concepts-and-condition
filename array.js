492511996dfc6f3ac5877ce79c83b1c7acea3dc7
rasel726 <md.rasel2726@gmail.com>
2022-08-03 23:51:26 +0600

    array file added

---
+++ array.js
@@ -0,0 +1,6 @@
+var friendAge = [11, 21, 45, 17, 14, 105, 14];
+var picnicFree = [5000, 2000, 4000, 150];
+var nayikas = ['mahi', 'opu', 'sabnur', 'sabana'];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(friendAge.length);
