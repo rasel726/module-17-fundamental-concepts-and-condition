dabb41d03ddcc49ac5a102411f7c58a6d6ee377d
rasel726 <md.rasel2726@gmail.com>
2022-08-03 23:17:29 +0600

    variable file added

---
+++ variable.js
@@ -0,0 +1,31 @@
+// number type variable
+var sunglassPrice = 120;
+
+// string type variable
+var specialOneName = "Rasel Sarkar";
+
+// boolean type variable
+var isSingle = true;
+
+// two types of number: Integer : 120,
+// float, decimal: 3.65,
+
+// variable naming convention: rules, best practice
+/**
+ * 1. single word 
+ * 2. no gap or space
+ * 3. no quote
+ * 4. no keyword
+ * 5. can not start with a number
+ * 6. can not contain dash
+ * 7. should use camelCase
+*/
+
+// math operations: +, -, *, /
+// shorthand: +=, -=, ++, --
+
+// convert: parseInt, parseFloat
+
+// toFixed
+
+// reminder: vagses: %
