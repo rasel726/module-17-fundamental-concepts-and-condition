9791cfcad36c3bd8db908b255c0c67c5b8cb3cc4
rasel726 <md.rasel2726@gmail.com>
2022-08-05 00:06:12 +0600

    multi stage conditions file added

---
+++ multi-stage-conditions.js
@@ -0,0 +1,17 @@
+var money = 68;
+var danishPrice = 45;
+var butterBread = 35;
+var tostBiscuit = 20;
+
+if(danishPrice < money){
+    console.log('Dan Dan Danish Khabo');
+}
+else if(butterBread < money){
+    console.log('Butter bon khabo');
+}
+else if(tostBiscuit < money){
+    console.log('Chubay chubay tost biscuit khabo');
+}
+else{
+    console.log('khali chaye sasther jonno valo');
+}
