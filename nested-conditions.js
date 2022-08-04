90cee41c934fd69854b98b28d3debd53da3dc6b1
rasel726 <md.rasel2726@gmail.com>
2022-08-05 00:16:53 +0600

    nested-conditions file added

---
+++ nested-conditions.js
@@ -0,0 +1,17 @@
+var  math = true;
+var geomatry = true;
+var straightLint = flase;
+
+if(math == true){
+    if(geomatry == true){
+        if(straightLint == true){
+
+        }
+        else{
+            console.log('baka pothe cholo');
+        }
+    }
+    else{
+        console.log('Pithagoras hoite parbana');
+    }
+}
