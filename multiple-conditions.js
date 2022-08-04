e006b79871074b8a3aca74b190c7198aa7503bdb
rasel726 <md.rasel2726@gmail.com>
2022-08-04 23:47:06 +0600

    multiple-conditions file added

---
+++ multiple-conditions.js
@@ -0,0 +1,34 @@
+// var isGraduatate = true;
+// var salary = 75000;
+// var cars = 1;
+// if(isGraduatate ===  true){
+//     console.log('Eso biye kore feli');
+// }
+// else{
+//     console.log('tor kopale biye nai');
+// }
+
+// if(isGraduatate ===  true && salary > 50000){
+//     console.log('Eso biye kore feli');
+// }
+// else{
+//     console.log('tor kopale biye nai');
+// }
+
+// if(isGraduatate ===  true && salary > 50000 && cars >= 1){
+//     console.log('Eso biye kore feli');
+// }
+// else{
+//     console.log('tor kopale biye nai');
+// }
+
+var isGraduatate = false;
+var salary = 61000;
+var cars = 0;
+
+if(isGraduatate ===  true || (salary > 50000 || cars >= 1)){
+    console.log('Eso prem kori');
+}
+else{
+    console.log('tomar friend er mobile number dao');
+}
