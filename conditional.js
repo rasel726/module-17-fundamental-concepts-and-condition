57ab533619c15bff420552c35b09ad6d8de855bb
rasel726 <md.rasel2726@gmail.com>
2022-08-04 23:16:50 +0600

    conditional file added

---
+++ conditional.js
@@ -0,0 +1,39 @@
+var iphonePrice = 79000;
+var myBudget = 95000;
+// if iphone is less than my budget. I will buy the iphone
+/**
+ * if iphone is less than my budget
+ *      i will buy the iphone
+ * 
+ * 
+ * if (condition){
+ *      // will executive if the conditon is true
+ * }
+ */
+
+if (iphonePrice < myBudget){
+    console.log('iphone kine futani marbo');
+}
+
+var chickenPrice = 500;
+var myMoney = 50;
+// if (chickenPrice <= myMoney){
+//     console.log('Murgir ran khamu and haddi chibamu');
+// }
+// if (chickenPrice > myMoney){
+//     console.log('Smashed potato and lentils soup');
+// }
+
+/**
+ * if chiken price is less than my budget
+ *      i will eat chiken
+ * na hoy: else
+ * 
+ */
+
+if(chickenPrice < myMoney){
+    console.log('I will eat chiken');
+}
+else{
+    console.log('I will eat potato');
+}
