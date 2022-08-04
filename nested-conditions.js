e091cd2b34441d82ee0af79f2ca403ee23c045d9
rasel726 <md.rasel2726@gmail.com>
2022-08-05 00:55:55 +0600

    summary file added

--- nested-conditions.js
+++ nested-conditions.js
@@ -5,7 +5,7 @@ var straightLint = flase;
 if(math == true){
     if(geomatry == true){
         if(straightLint == true){
-
+            
         }
         else{
             console.log('baka pothe cholo');
